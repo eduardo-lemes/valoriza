b8ffc54647a8eb86a0efaa85f84a24c37e8b2301
eduardoLemes <eduardo_lsf@outlook.com>
2021-06-24 01:10:21 -0300

    Migrations created and testing

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
+import "reflect-metadata";
 import express, { request } from "express";
+
+import "./database";
 
 const app = express();
 
-app.get("/test", (request, response) => {
-    return response.send("Olá teste")
-});
 
-app.post("/test-post", (request, response) => {
-    return response.send("Olá test post")
-});
 
 app.listen(3000, () => console.log("Server is running!"))
